18ee6cf8671fbd301e24193ced4214357900c523
jimangel2001 <jimangel2001@yahoo.gr>
2019-04-02 15:05:13 +0300

    Fixed issue with the order the files were merged, and a geolocation issue where some link would not lead to the correct .m3u8 file

--- ert-downloader.py
+++ ert-downloader.py
@@ -6,9 +6,9 @@ import sys
 
 if __name__ == "__main__":
 
-    title = ''
     url_root = ''
     url = ''
+    text = ''
     files = []
     page_url = sys.argv[1]  # website url
     threads = 5  # amount of workers who are downloading at the same time
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     for i in video_file:  # getting the .mp4 link
         if '.mp4' in i:
             url_root = (i.split("'")[1].split('.mp4')[0]+'.mp4/')  # find the link we will use to get the .m3u8 link
+            url_root = url_root.replace('dvrorigin/', 'dvrorigingr/')  # repairing the link, probably geo issue
             break
 
     playlist_m3u8 = requests.get(url_root+'playlist.m3u8', verify=False).text  # this file contains
@@ -39,13 +40,13 @@ if __name__ == "__main__":
     for i in chunklist_lines:
         if '.ts' in i:  # we get each segment filename that's used to generate the urls of each
             files.append(i)
-    text = ''
+            text += 'file ' + i + '\n'
 
+    w = open('filenames.txt', 'w+')  # the file with the filenames for the ffmpeg
+    w.write(text)
+    w.close()
 
     def download(link):
-        global text  # this text contains the segment names and is used in the ffmpeg
-            # to join them to generate the final vide
-        text += 'file ' + link + '\n'
         with open(link, 'wb') as f:
             file = requests.get(url_root+link, verify=False)
             f.write(file.content)
@@ -54,10 +55,6 @@ if __name__ == "__main__":
     thread = pool.map(download, files)
     pool.close()
     pool.join()  # waiting for the downloads to complete
-
-    w = open('filenames.txt', 'w+')  # the file with the filenames for the ffmpeg
-    w.write(text)
-    w.close()
 
     os.system(('ffmpeg -f concat -i filenames.txt -acodec copy -vcodec copy {0}.mp4').format(title))   # finally we can
     # GENERATE THE VIDEO
