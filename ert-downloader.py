a35ca5507c6ad0908aecd31d1be5b56b662bb8e4
AngelouD <jimangel2001@gmail.com>
2020-03-22 22:26:57 +0200

    add support for archive.ert

--- ert-downloader.py
+++ ert-downloader.py
@@ -21,15 +21,14 @@ if __name__ == "__main__":
         if '.mp4' in i:  # finds the url in the source code of the video that contains the
             url = i      # link for the stream link (.mp4) that contains the .m3u8 files
             break
-
-    video_url = url.split('.mp4')[0][13:]+'.mp4'  # extract the forementioned link
-
+    video_url = re.split('src="|&', url)[1]  # extract the forementioned link
     video_file = requests.get(video_url).text.split('\n')  # reading the 'video file' that contains the necessary link
 
     for i in video_file:  # getting the .mp4 link
         if '.mp4' in i:
             url_root = (i.split("'")[1].split('.mp4')[0]+'.mp4/')  # find the link we will use to get the .m3u8 link
-            break
+            if("ep.ert.gr/" in url_root):
+                break
 
     playlist_m3u8 = requests.get(url_root+'playlist.m3u8', verify=False).text  # this file contains
     # the link for the file that contains the links of each video segment
