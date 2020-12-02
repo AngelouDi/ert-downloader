58570dfb277c0a0ffdba973f3f420de7831216f9
AngelouD <jimangel2001@gmail.com>
2020-12-02 15:40:12 +0200

    Fixed mistakes in code

--- extractors/archive_extractor.py
+++ extractors/archive_extractor.py
@@ -2,36 +2,40 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-
-def obtain_title(url):
+def obtain_html(url):
     html = requests.get(url).text
+    return html
+
+def obtain_title(html):
     soup = BeautifulSoup(html, 'lxml')
     title = soup.find('title').text
     title = re.sub(":|/|\||\"", "-", title);
     return title
 
-def obtain_player_url(url):
-    html = requests.get(url).text
+
+def obtain_player_url(html):
     soup = BeautifulSoup(html, 'lxml')
     player_iframe = soup.find('iframe')
     player_url = re.split('&', (player_iframe.attrs["src"]))[0]
     return player_url
 
 
-def obtain_stream_url(url):
+def obtain_geo(html):
+    is_geo = re.search("geo=(.*?)&", html)
+    return is_geo
+
+
+def obtain_stream_url(url, is_geo):
     html = requests.get(url).text
     stream_url = re.split("var HLSLink = '(.*)'", html)[1]
-    is_geo = re.split("var isGeo = \"(.*)\"", html)[1]
-    if("dvrorigingr" not in stream_url and is_geo  == "true"):
+    if("dvrorigingr" not in stream_url and is_geo):
         stream_url = re.sub("dvrorigin", "dvrorigingr", stream_url)
     stream_url = re.sub("/playlist.m3u8", "", stream_url)
-    print(stream_url)
     return stream_url
 
 
 def obtain_chunklist(url):
     chunklist_url = url + "/chunklist.m3u8"
-    print(chunklist_url)
     clean_chunklist = []
     chunklist = requests.get(chunklist_url).text.split('\n')
     for chunk in chunklist:
@@ -41,7 +45,9 @@ def obtain_chunklist(url):
 
 
 def obtain_data(url):
-    title = obtain_title(url)
-    player_url = obtain_player_url(url)
-    stream_url = obtain_stream_url(player_url)
+    html = obtain_html(url)
+    title = obtain_title(html)
+    player_url = obtain_player_url(html)
+    is_geo = obtain_geo(html)
+    stream_url = obtain_stream_url(player_url, is_geo)
     return {"title": title, "stream_url": stream_url, "chunklist": obtain_chunklist(stream_url)}
